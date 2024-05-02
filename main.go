d0778662644712259eba75d22067abd0d3c12aa4
Nick Econopouly <nick@mail.wrycode.com>
2024-05-02 11:47:21 -0400

    Render multiple words

--- main.go
+++ main.go
@@ -11,18 +11,23 @@ import (
 
 func main() {
 	// Create new canvas of dimension 100x100 mm
-	c := canvas.New(500, 500)
+	c := canvas.New(200, 200)
 
 	// Create a canvas context used to keep drawing state
 	ctx := canvas.NewContext(c)
-
-	var Transparent = color.RGBA{0x00, 0x00, 0x00, 0x00} // rgba(0, 0, 0, 0)
+	var Transparent = color.RGBA{0x00, 0x00, 0x00, 0x00} // rgba(0, 0, 0, 0)	
+	ctx.SetFillColor(Transparent)
+	ctx.SetStrokeColor(canvas.Black)
 	
 	data, err := ioutil.ReadFile("forms.json")
 	if err != nil {
 		fmt.Println("Error reading file:", err)
 		return
 	}
+
+	// point
+	pos := canvas.Point{X: 10, Y: 100}
+
 	// Unmarshal the JSON data into a map
 	var formsMap map[string]string
 	err = json.Unmarshal(data, &formsMap)
@@ -31,23 +36,32 @@ func main() {
 		return
 	}
 
-	// Create an SVG path from the JSON form dictionary and draw it to the canvas
-	form, err := canvas.ParseSVGPath(formsMap["l"])
-	if err != nil {
-		panic(err)
+	// Simple sentence: "Welcome to a new way to write"
+	message := [][]string{
+		{"w", "e", "l", "c", "u", "m"}, 
+		{"t"},
+		{"u"},
+		{"n", "oo"},
+		{"w", "ae"},
+		{"t"},
+		{"r", "wr_i_te", "t"},
 	}
-	ctx.SetFillColor(Transparent)
-	ctx.SetStrokeColor(canvas.Black)
-	ctx.DrawPath(150, 150, form)
 
-	form2, err := canvas.ParseSVGPath(formsMap["g"])
-	if err != nil {
-		panic(err)
+	// Render each word
+	for _, word := range message {
+
+		for _, form := range word {
+			formPath, err := canvas.ParseSVGPath(formsMap[form])
+			if err != nil {
+				panic(err)
+			}
+			ctx.DrawPath(pos.X, pos.Y, formPath)
+			pos.X += formPath.Pos().X
+			pos.Y += formPath.Pos().Y
+		}
+		pos.X += 10
 	}
-	ctx.SetFillColor(Transparent)
-	ctx.SetStrokeColor(canvas.Black)
-	ctx.DrawPath(176.30212, 150.23228, form2)
-// 26.30212,0.23228
+
 	// Rasterize the canvas and write to a PNG file with 3.2 dots-per-mm (320x320 px)
 	if err := renderers.Write("rendered_text.png", c, canvas.DPMM(3.2)); err != nil {
 		panic(err)
