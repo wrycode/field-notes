3ca3e7103b9697ec6ce6aa00edf04dc239fa48f1
Nick Econopouly <nick@mail.wrycode.com>
2024-05-04 12:48:41 -0400

    Retrieve forms directly from one SVG file using 'IPA: ' label prefix

--- go.mod
+++ go.mod
@@ -4,7 +4,10 @@ go 1.22.0
 
 toolchain go1.22.2
 
-require github.com/tdewolff/canvas v0.0.0-20240420213651-d5a04e36ef50
+require (
+	github.com/beevik/etree v1.3.0
+	github.com/tdewolff/canvas v0.0.0-20240502214346-a72e4acc2272
+)
 
 require (
 	github.com/ByteArena/poly2tri-go v0.0.0-20170716161910-d102ad91854f // indirect
@@ -12,16 +15,17 @@ require (
 	github.com/benoitkugler/textlayout v0.3.0 // indirect
 	github.com/benoitkugler/textprocessing v0.0.3 // indirect
 	github.com/go-fonts/latin-modern v0.3.2 // indirect
+	github.com/go-fonts/liberation v0.3.2 // indirect
 	github.com/go-latex/latex v0.0.0-20231108140139-5c1ce85aa4ea // indirect
 	github.com/go-text/typesetting v0.1.1 // indirect
 	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
-	github.com/tdewolff/font v0.0.0-20240417221047-e5855237f87b // indirect
+	github.com/tdewolff/font v0.0.0-20240502124818-41eff0ab0cd8 // indirect
 	github.com/tdewolff/minify/v2 v2.20.20 // indirect
 	github.com/tdewolff/parse/v2 v2.7.13 // indirect
 	github.com/wcharczuk/go-chart/v2 v2.1.1 // indirect
 	golang.org/x/image v0.15.0 // indirect
 	golang.org/x/net v0.24.0 // indirect
-	golang.org/x/text v0.14.0 // indirect
+	golang.org/x/text v0.15.0 // indirect
 	gonum.org/v1/plot v0.14.0 // indirect
 	star-tex.org/x/tex v0.4.0 // indirect
 )
--- main.go
+++ main.go
@@ -3,48 +3,66 @@ package main
 import (
 	"github.com/tdewolff/canvas"
 	"github.com/tdewolff/canvas/renderers"
-	"encoding/json"
+	// "encoding/json"
 	"fmt"
-	"io/ioutil"
+	// "io/ioutil"
 	"image/color"
+	// "strings"
+	// "os"
+	// "encoding/xml"
+	"github.com/beevik/etree"	
+	"log"
+	"strings"
+	// "reflect"
 )
 
 func main() {
+	
 	// Create new canvas of dimension 100x100 mm
-	c := canvas.New(200, 200)
+	c := canvas.New(800, 800)
 
 	// Create a canvas context used to keep drawing state
 	ctx := canvas.NewContext(c)
 	var Transparent = color.RGBA{0x00, 0x00, 0x00, 0x00} // rgba(0, 0, 0, 0)	
 	ctx.SetFillColor(Transparent)
 	ctx.SetStrokeColor(canvas.Black)
+
+	formsMap := make(map[string]string)
 	
-	data, err := ioutil.ReadFile("forms.json")
-	if err != nil {
-		fmt.Println("Error reading file:", err)
-		return
+
+	doc := etree.NewDocument()
+	if err := doc.ReadFromFile("lang/system.svg"); err != nil {
+		log.Fatalf("Failed to parse document: %v", err)
 	}
 
+	root := doc.SelectElement("svg")
+	for _, path_element := range root.FindElements("//path") {
+		label := path_element.SelectAttrValue("inkscape:label", "unknown")
+		if strings.HasPrefix(label, "IPA:") {
+			standard_IPA_prununciation  := strings.TrimPrefix(label, "IPA: ")
+			SVG_path := path_element.SelectAttr("d").Value
+			// TODO: remove the first 'move' command to make these paths relative
+			formsMap[standard_IPA_prununciation] = SVG_path
+		}
+	}
+
+	for k, v := range formsMap {
+		fmt.Println("IPA: ", k)
+		fmt.Println("Path: ", v)
+	}
+	
 	// point
 	pos := canvas.Point{X: 10, Y: 100}
 
-	// Unmarshal the JSON data into a map
-	var formsMap map[string]string
-	err = json.Unmarshal(data, &formsMap)
-	if err != nil {
-		fmt.Println("Error unmarshaling JSON:", err)
-		return
-	}
-
 	// Simple sentence: "Welcome to a new way to write"
 	message := [][]string{
-		{"w", "e", "l", "c", "u", "m"}, 
+		{"w", "ɛ", "l", "c", "ʌ", "m"}, 
 		{"t"},
-		{"u"},
+		{"ʌ"},
 		{"n", "oo"},
-		{"w", "ae"},
+		{"w", "eɪ"},
 		{"t"},
-		{"r", "wr_i_te", "t"},
+		{"r", "aɪ", "t"},
 	}
 
 	// Render each word
