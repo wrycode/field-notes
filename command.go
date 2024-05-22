f39ab22fb7fb21155f663dff3536b8458267cfdc
Nick Econopouly <nick@mail.wrycode.com>
2024-05-22 15:18:35 -0400

    cleanup

--- command.go
+++ command.go
@@ -24,24 +24,11 @@ func fileToString(filename string) string {
 }
 
 func main() {
-
-	// Create new canvas
 	c := canvas.New(500,500)
 	l := log.Default()
 	l.SetFlags(0)		// remove timestamp
 	options := fileToString("options.json")
-	// options := `{
-	// "Script_debug": false,
-	// "Custom_script_svg_value":  "",
-	// "Builtin_script_name": "teen_script.svg",
-	// "Language_code": "en_US",
-	// "Image_width": 500,
-	// "Input_text": "Reading rainbow! How are you doing? Let's see how well we can do at testing logographs! This is not my forte, but I just want you to know about my system and what you can do with this",
-	// }`
-
 	Render(options, c, l)
-
-	// Rasterize the canvas and write to a PNG file with 3.2 dots-per-mm (320x320 px)
 	if err := renderers.Write("rendered_text.png", c, canvas.DefaultResolution); err != nil {
 		fmt.Println("tes")
 		panic(err)
